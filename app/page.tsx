77421578ba28545ca30ac223a40c19d79e3958f1
mariofredooo <mariotannuwijaya@gmail.com>
2025-10-08 13:21:55 +0700

    feat: modify sidebar page ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 import Image from 'next/image';
+import Link from 'next/link';
+import clsx from 'clsx';
 
 export default function Home() {
-  return <div className="">WELCOME TO PLAYGROUND</div>;
+  return (
+    <div className={clsx(`flex flex-col gap-[20px]`)}>
+      WELCOME TO PLAYGROUND
+      <Link href={'/component/sidebar'}>Sidebar</Link>
+    </div>
+  );
 }
--- components/sidebar/sidebar.tsx
+++ components/sidebar/sidebar.tsx
@@ -16,20 +16,21 @@ export default function Sidebar({ logo }: SidebarProps) {
     { label: 'Profile', href: '/profile' },
   ];
   return (
-    <div className={clsx(`sticky top-0 h-screen w-[160px] bg-gray-800 text-white p-[20px]`)}>
+    <div className={clsx(`sticky top-0 h-screen w-auto w-max-[320px] bg-gray-800 text-white p-[20px]`)}>
       {/* <Image className={clsx(`w-full h-auto rounded-[10px]`)} src={logo} alt="logo" /> */}
-      <ul className={clsx(`mt-[20px] list-none px-[10px]`)}>
+      <ul className={clsx(`list-none !p-[20px]`)}>
         {sidebarItems.map((item) => (
           <li className={clsx(`my-[10px] text-[18px] flex justify-center`)} key={item.label}>
-            <Link
-              href={item.href}
-              className={clsx(
-                `inline-flex justify-center items-center gap-[10px] text-white font-semibold text-[20px]`,
-                `w-[40px] h-[40px] rounded-[10px] overflow-hidden`,
-              )}
-            >
-              {item.icon && item.icon}
-              <span className="hidden">{item.label}</span>
+            <Link href={item.href} className={clsx(``)}>
+              <div
+                className={clsx(
+                  `inline-flex justify-center items-center gap-[10px] text-white font-semibold text-[20px] mx-[10px] transition-all duration-300 ease-in-out`,
+                  `w-[40px] h-[40px] rounded-[10px] hover:w-[360px] hover:overflow-visible`,
+                )}
+              >
+                {item.icon && item.icon}
+                <span className="">{item.label}</span>
+              </div>
             </Link>
           </li>
         ))}
