fc45e6ecb6e38e5df191f3f771effe4ec897f3b1
mariofredo <mariotannuwijaya@gmail.com>
2026-03-24 17:02:47 +0700

    feat: update .prettierrc for tailwindcss support, enhance page layout, and upgrade next-auth dependency

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import Image from 'next/image';
 import Link from 'next/link';
+
 import clsx from 'clsx';
 
 export default function Home() {
   return (
-    <div className={clsx(`flex flex-col gap-[20px]`)}>
-      WELCOME TO PLAYGROUND
-      <Link href={'/component/sidebar'}>Sidebar</Link>
+    <div className={clsx(`flex h-screen flex-col items-center justify-center gap-[20px]`)}>
+      <h1 className={clsx(`text-[32px] font-bold`)}>Welcome to Next Template</h1>
     </div>
   );
 }
